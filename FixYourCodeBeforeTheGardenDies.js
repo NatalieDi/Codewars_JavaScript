ce04ebcc386545ce3e4506f00a4907f72b42d8e9
NatalieDiekmann <nataliia.diekmann@gmail.com>
2023-11-06 17:57:47 -0600

    Added links and 3 new tasks (#3)

--- FixYourCodeBeforeTheGardenDies.js
+++ FixYourCodeBeforeTheGardenDies.js
@@ -1,3 +1,4 @@
+//https://www.codewars.com/kata/57158fb92ad763bb180004e7/train/javascript 
 //Description
 //You have an award-winning garden and every day the plants need exactly 40mm of water. 
 //You created a great piece of JavaScript to calculate the amount of water your plants will need when you have taken into consideration 
---
+++ OppositeNumber.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
+//Description
+//Very simple, given an integer or a floating-point number, find its opposite.
+//Examples:
+//1: -1
+//14: -14
+//-34: 34
+function opposite(number) {
+    return -number
+}
+console.log(opposite(1));
+console.log(opposite(14));
+console.log(opposite(-34));
