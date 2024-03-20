79cd0efa7c7b5629003f55fcd37726a3c65671da
NatalieD <qa.job.natalie.d@gmail.com>
2024-03-20 11:19:22 -0500

    Changed to ternarny

--- 8 kyu/ConvertBooleanValuesToStrings.js
+++ 8 kyu/ConvertBooleanValuesToStrings.js
@@ -3,8 +3,9 @@
 //Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
 function boolToWord( bool ){
-    if ( bool == true) {return "Yes";}
-        else {return "No";}
+    // if ( bool == true) {return "Yes";}
+    //     else {return "No";}
+    return bool == true ? "Yes" : "No";
 }
-console.log(boolToWord(true, 'Yes'));
-console.log(boolToWord(false, 'No'));
+console.log(boolToWord(10>5, 'Yes'));
+console.log(boolToWord(10<5, 'No'));
