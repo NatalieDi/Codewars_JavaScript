4278287d0f0016996e1dfc22acbce3b38deb0e44
NatalieD <qa.job.natalie.d@gmail.com>
2024-03-20 12:05:59 -0500

    changed to ternary to ReturnNegative

--- 8 kyu/ReturnNegative.js
+++ 8 kyu/ReturnNegative.js
@@ -12,8 +12,7 @@
 //Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
 
 function makeNegative(num) {
-    if (num > 0){return -num;}
-    else return num;
+    return num > 0 ? -num: num;
 }
 console.log(makeNegative(1));
 console.log(makeNegative(-5));
