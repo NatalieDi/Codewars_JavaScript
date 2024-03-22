a02dea638cc0219e66cd2b7cc1fc6879b4e979a2
NatalieD <qa.job.natalie.d@gmail.com>
2024-03-22 00:32:55 -0500

    solution 2 positiveSum

--- 8 kyu/Sumofpositive.js
+++ 8 kyu/Sumofpositive.js
@@ -13,7 +13,7 @@ Solution 1*/
 // }
 /*Solution 2*/
 function positiveSum(arr) {
-    return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
+    return arr.reduce((a,b) => a + (b > 0 ? b : 0), 0);
  }
 
 console.log(positiveSum([1,-4,7,12])); //20
