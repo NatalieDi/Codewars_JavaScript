60983baf2adb90fc464c86d15f146ef3de97d799
NatalieD <qa.job.natalie.d@gmail.com>
2024-03-22 23:28:44 -0500

    sayHello

---
+++ 8 kyu/WelcometotheCity.js
@@ -0,0 +1,18 @@
+/*https://www.codewars.com/kata/5302d846be2a9189af0001e4/train/javascript
+Description
+Create a method that takes as input a name, city, and state to welcome a person. 
+Note that name will be an array consisting of one or more values that should be 
+joined together with one space between each, and the length of the name array 
+in test cases will vary.
+Example:
+['John', 'Smith'], 'Phoenix', 'Arizona'
+*/
+function sayHello( name, city, state ) {
+
+    return "Hello, " + name.join(" ") + "! Welcome to " + city + ", "+ state + "!"
+   }
+
+console.log(sayHello(['John', 'Smith'], 'Phoenix', 'Arizona')); // 'Hello, John Smith! Welcome to Phoenix, Arizona!'
+console.log(sayHello(['Franklin','Delano','Roosevelt'], 'Chicago', 'Illinois')); // 'Hello, Franklin Delano Roosevelt! Welcome to Chicago, Illinois!'
+console.log(sayHello(['Wallace','Russel','Osbourne'],'Albany','New York')); // 'Hello, Wallace Russel Osbourne! Welcome to Albany, New York!'
+  
